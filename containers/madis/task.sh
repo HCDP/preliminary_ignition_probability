10dafd2a0226ca9567843266219b3d366897b742
jardmclan <mcleanj@hawaii.edu>
2024-02-14 14:29:38 -1000

    add containers, add wrokflows

---
+++ containers/madis/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "[task.sh] [1/4] Starting Execution."
+
+echo "[task.sh] [2/4] Collecting Climate data from MADIS on the daily timeframe."
+cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/madis
+python3 mesonet_24hr_fetch_dev.py
+python3 hfmetar_24hr_fetch_dev.py
+
+echo "[task.sh] [3/4] Injecting authentication variables for uploader."
+cd /sync
+cp upload_config.json config.json
+python3 add_auth_info_to_config.py config.json
+
+echo "[task.sh] [4/4] Attempting to upload the gathered data."
+python3 upload.py
+
+echo "[task.sh] All done!"
