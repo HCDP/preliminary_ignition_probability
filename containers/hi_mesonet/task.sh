614c2a434df1820cc4eb2e8d178d16b9a1ac9937
jardmclan <mcleanj@hawaii.edu>
2024-04-22 10:21:34 -1000

    standardize custom date env var

--- containers/hi_mesonet/task.sh
+++ containers/hi_mesonet/task.sh
@@ -2,20 +2,20 @@
 echo "[task.sh] [1/4] Starting Execution."
 export TZ="HST"
 echo "It is currently $(date)."
-if [ $ACQUISITION_DATE ]; then
+if [ $CUSTOM_DATE ]; then
     echo "An Acquisition date was provided by the environment."
 else
-    export ACQUISITION_DATE=$(date -d "1 day ago" --iso-8601)
+    export CUSTOM_DATE=$(date -d "1 day ago" --iso-8601)
     echo "No Acquisition date was provided by the environment. Defaulting to yesterday."
 fi
-echo "Acquisition date is: " $ACQUISITION_DATE
+echo "Acquisition date is: " $CUSTOM_DATE
 
 echo "[task.sh] [2/4] Collecting Climate data from Synoptic on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/hi_mesonet
 echo "---hiMeso_24hr_data_aqs.R---"
-Rscript hiMeso_24hr_data_aqs.R $ACQUISITION_DATE
+Rscript hiMeso_24hr_data_aqs.R $CUSTOM_DATE
 echo "---hiMeso_synoptic_24hr_data_aqs.R---"
-Rscript hiMeso_synoptic_24hr_data_aqs.R $ACQUISITION_DATE
+Rscript hiMeso_synoptic_24hr_data_aqs.R $CUSTOM_DATE
 
 echo "[task.sh] [3/4] Injecting authentication variables for uploader."
 cd /sync
