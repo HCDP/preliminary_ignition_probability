a7c7ef8fda122c4ca2764031638d20611139a400
jardmclan <mcleanj@hawaii.edu>
2024-07-23 09:02:20 -1000

    add date processing to nws

--- containers/nws_rr5/task.sh
+++ containers/nws_rr5/task.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 echo "[task.sh] [1/4] Starting Execution."
+export TZ="HST"
+echo "It is currently $(date)."
+if [ $CUSTOM_DATE ]; then
+    echo "An Acquisition date was provided by the environment."
+else
+    export CUSTOM_DATE=$(date -d "1 day ago" --iso-8601)
+    echo "No Acquisition date was provided by the environment. Defaulting to yesterday."
+fi
+echo "Acquisition date is: " $CUSTOM_DATE
 
 echo "[task.sh] [2/4] Collecting Climate data from NWS on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/nws_rr5
