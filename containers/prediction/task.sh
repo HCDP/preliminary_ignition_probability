171eac8e3975d42062ccb981ac943635069ab7ab
jardmclan <mcleanj@hawaii.edu>
2025-04-29 11:00:34 -1000

    dir fixes

--- containers/prediction/task.sh
+++ containers/prediction/task.sh
@@ -13,16 +13,16 @@ echo "Aggregation date is: " $CUSTOM_DATE
 source /workspace/envs/prod.env
 
 echo "[task.sh] [2/6] Fetching dependencies, all counties."
-python3 -W ignore /workspace/code/wget_dependencies.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/prediction/wget_dependencies.py $CUSTOM_DATE
 
 echo "[task.sh] [3/6] Running map workflow, all counties"
-python3 -W ignore /workspace/code/generate_ignition_forecast.py bi $CUSTOM_DATE
-python3 -W ignore /workspace/code/generate_ignition_forecast.py ka $CUSTOM_DATE
-python3 -W ignore /workspace/code/generate_ignition_forecast.py mn $CUSTOM_DATE
-python3 -W ignore /workspace/code/generate_ignition_forecast.py oa $CUSTOM_DATE
+python3 -W ignore /workspace/code/prediction/generate_ignition_forecast.py bi $CUSTOM_DATE
+python3 -W ignore /workspace/code/prediction/generate_ignition_forecast.py ka $CUSTOM_DATE
+python3 -W ignore /workspace/code/prediction/generate_ignition_forecast.py mn $CUSTOM_DATE
+python3 -W ignore /workspace/code/prediction/generate_ignition_forecast.py oa $CUSTOM_DATE
 
 echo "[task.sh] [4/6] Creating statewide mosaic."
-python3 -W ignore /workspace/code/statewide_mosaic.py
+python3 -W ignore /workspace/code/prediction/statewide_mosaic.py
 
 echo "[task.sh] [5/6] Preparing upload config."
 cd /sync
